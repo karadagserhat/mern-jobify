a89c4896abe9872795d9788cfa9d3b9ac1fb82a2
karadagserhat <serhatkaradagg@outlook.com>
2023-07-19 17:44:53 +0300

    first commit

---
+++ client/src/pages/Landing.jsx
@@ -0,0 +1,35 @@
+import Wrapper from '../assets/wrappers/LandingPage';
+import main from '../assets/images/main.svg';
+import { Link } from 'react-router-dom';
+import { Logo } from '../components';
+
+function Landing() {
+  return (
+    <Wrapper>
+      <nav>
+        <Logo />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum,
+            corporis qui a consectetur similique vero. Lorem ipsum dolor sit
+            amet consectetur adipisicing elit. Vero!
+          </p>
+          <Link to="/register" className="btn register-link">
+            Register
+          </Link>
+          <Link to="/login" className="btn">
+            Login / Demo User
+          </Link>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </Wrapper>
+  );
+}
+
+export default Landing;
